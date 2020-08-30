a33f231f669b2feb0c9290e5a3bc52ebf7681fae
mwingfield <mwingfie@gmail.com>
2020-08-30 00:17:59 -0600

    v0.0.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 echo "Hello $1 $2"
 
 echo " - ls -la ~/haxelib"
-ls -la ~/haxelib
+ls -la /var/haxelib
 # not sure why it is having me do this agian, but it is
 echo " - haxelib setup ~/haxelib"
-haxelib setup ~/haxelib
+haxelib setup /var/haxelib
 echo " - haxelib version"
 haxelib version
 echo " - haxelib path lime"
