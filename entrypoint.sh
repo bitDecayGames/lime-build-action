4d2b6405d7a1efc9bc69dbb297977519dab1f3aa
mwingfield <mwingfie@gmail.com>
2020-08-29 23:48:09 -0600

    add echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
 echo "Hello $1 $2"
+# not sure why it is having me do this agian
+echo "haxelib setup ~/haxelib"
+haxelib setup ~/haxelib
+echo "haxelib version"
 haxelib version
+echo "lime --version"
 lime --version
