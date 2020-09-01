51fcfdd43924bd7a15532277489e2c1fffe1c515
mwingfield <mwingfie@gmail.com>
2020-08-31 21:47:18 -0600

    v0.0.24

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,13 @@
 # first argument is the sub-directory (defaults to ./)
 # second argument is the platform for the lime build (defaults to html5)
 # third argument is build args to the build call
+# forth argument is a | separated list of git dependencies for haxelib to install
 
 set -e
 
 echo "ls -la /var/haxelib"
 ls -la /var/haxelib
+
 # not sure why it is having me do this agian, but it is
 echo "haxelib setup /var/haxelib"
 haxelib setup /var/haxelib
@@ -23,6 +25,15 @@ ls -la
 
 echo "haxelib install all"
 yes y | haxelib install all
+
+set +e
+IFS='|' read -r -a gitDependencyArray <<< "$4"
+for gitDep in "${gitDependencyArray[@]}"
+do
+	echo "haxelib git $gitDep"
+    haxelib git $gitDep
+done
+set -e
 
 echo "lime build $2 $3"
 lime build $2 $3
